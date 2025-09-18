8911533d5f65f6d5d660177477d1e95ecfc0dc4f
TajanaSA <tajana.salkic@skyline.be>
2025-09-18 09:53:50 +0200

    Added sorting to Interface table and added a display key, removed the unnecessary log lines in QAction_2 and changed the types of extendedSpeed and interfaceSpeed from var to object

--- QAction_2/QAction_2.cs
+++ QAction_2/QAction_2.cs
@@ -18,20 +18,15 @@ public static class QAction
 			string rowPK = protocol.RowKey();
 			int oneBasedOffSet = 1;
 
-			var interfaceSpeed = GetInterfaceSpeed(protocol, rowPK, oneBasedOffSet);
-
-			var extendeSpeed = protocol.GetParameterIndexByKey(Parameter.Extendedinterfacetable.tablePid, rowPK, Parameter.Extendedinterfacetable.Idx.extendedinterfacetablehighspeed_1402 + 1);
-			protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}| this is the interface row key: {rowPK} and speed: {interfaceSpeed}  and extended: {extendeSpeed}", LogType.Error, LogLevel.NoLogging);
-
+			uint interfaceSpeed = GetInterfaceSpeed(protocol, rowPK, oneBasedOffSet);
 			uint calculatedSpeed = GetCalculatedInterfaceSpeed(protocol, rowPK, interfaceSpeed, oneBasedOffSet);
 
 			bool hasSucceeded = protocol.SetParameterIndexByKey(Parameter.Interfacetable.tablePid, rowPK, Parameter.Interfacetable.Idx.interfacetablecalculatedinterfacespeed_1205 + oneBasedOffSet, calculatedSpeed);
 
 			if (!hasSucceeded)
 			{
-				protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Parameter set was not successful", LogType.Error, LogLevel.NoLogging);
+				protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Parameter Set was not successful", LogType.Error, LogLevel.NoLogging);
 			}
-
 		}
 		catch (Exception ex)
 		{
@@ -41,7 +36,7 @@ public static class QAction
 
 	private static uint GetInterfaceSpeed(SLProtocol protocol, string rowPK, int offset)
 	{
-		var interfaceSpeed = protocol.GetParameterIndexByKey(Parameter.Interfacetable.tablePid, rowPK, Parameter.Interfacetable.Idx.interfacetablespeed_1203 + offset);
+		object interfaceSpeed = protocol.GetParameterIndexByKey(Parameter.Interfacetable.tablePid, rowPK, Parameter.Interfacetable.Idx.interfacetablespeed_1203 + offset);
 
 		return Convert.ToUInt32(interfaceSpeed);
 	}
@@ -49,13 +44,14 @@ public static class QAction
 	private static uint GetCalculatedInterfaceSpeed(SLProtocol protocol, string rowPK, uint interfaceSpeed, int offset)
 	{
 		uint unsignedIntegerMaxValue = UInt32.MaxValue;
+
 		if (interfaceSpeed < unsignedIntegerMaxValue)
 		{
 			interfaceSpeed = interfaceSpeed / 1000000;
 		}
 		else if(interfaceSpeed == unsignedIntegerMaxValue)
 		{
-			var extendedSpeed = protocol.GetParameterIndexByKey(Parameter.Extendedinterfacetable.tablePid, rowPK, Parameter.Extendedinterfacetable.Idx.extendedinterfacetablehighspeed_1402 + offset);
+			object extendedSpeed = protocol.GetParameterIndexByKey(Parameter.Extendedinterfacetable.tablePid, rowPK, Parameter.Extendedinterfacetable.Idx.extendedinterfacetablehighspeed_1402 + offset);
 
 			interfaceSpeed = Convert.ToUInt32(extendedSpeed);
 		}
