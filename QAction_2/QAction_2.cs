ccc668bdbeabdb4d93d25e766a5f5951357ba3b8
TajanaSA <tajana.salkic@skyline.be>
2025-09-18 08:20:55 +0200

    Added getting a parameter value from a specific column and setting it a value to a specific column, changed the order of the groups in a timer

--- QAction_2/QAction_2.cs
+++ QAction_2/QAction_2.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 using Skyline.DataMiner.Net;
 using Skyline.DataMiner.Scripting;
-using Skyline.DataMiner.Utils.Protocol.Extension;
 
 /// <summary>
 /// DataMiner QAction Class: Calculate Interface Speed.
@@ -19,20 +15,37 @@ public static class QAction
 	{
 		try
 		{
-			//string rowPK = protocol.RowKey();
-			//var interfaceSpeed = protocol.GetCell(Parameter.Interfacetable.tablePid, rowPK, Parameter.Interfacetable.Idx.interfacetablespeed_2003);
-			protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}| this is the interface row key: and speed:", LogType.Error, LogLevel.NoLogging);
+			string rowPK = protocol.RowKey();
+			int oneBasedOffSet = 1;
+
+			var interfaceSpeed = protocol.GetParameterIndexByKey(Parameter.Interfacetable.tablePid, rowPK, Parameter.Interfacetable.Idx.interfacetablespeed_2003 + oneBasedOffSet);
+			uint convertedSpeed = Convert.ToUInt32(interfaceSpeed);
+
+			var extendeSpeed = protocol.GetParameterIndexByKey(Parameter.Extendedinterfacetable.tablePid, rowPK, Parameter.Extendedinterfacetable.Idx.extendedinterfacetablehighspeed_4002 + 1);
+			protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}| this is the interface row key: {rowPK} and speed: {interfaceSpeed}  and extended: {extendeSpeed}", LogType.Error, LogLevel.NoLogging);
 
 			var unsignedIntegerMaxValue = UInt32.MaxValue;
 
-			/*if (Convert.ToUInt32(interfaceSpeed) < unsignedIntegerMaxValue)
+			if (convertedSpeed < unsignedIntegerMaxValue)
 			{
+				bool hasSucceeded = protocol.SetParameterIndexByKey(Parameter.Interfacetable.tablePid, rowPK, Parameter.Interfacetable.Idx.interfacetablecalculatedinterfacespeed_2005 + oneBasedOffSet, convertedSpeed);
 
+				if (!hasSucceeded)
+				{
+					protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Exception thrown: Parameter set for Calculated Interface Speed was not successful", LogType.Error, LogLevel.NoLogging);
+				}
 			}
-			else
+			else if (convertedSpeed == unsignedIntegerMaxValue)
 			{
+				var extendedSpeed = protocol.GetParameterIndexByKey(Parameter.Extendedinterfacetable.tablePid, rowPK, Parameter.Extendedinterfacetable.Idx.extendedinterfacetablehighspeed_4002 + oneBasedOffSet);
 
-			}*/
+				bool hasSucceeded = protocol.SetParameterIndexByKey(Parameter.Interfacetable.tablePid, rowPK, Parameter.Interfacetable.Idx.interfacetablecalculatedinterfacespeed_2005 + oneBasedOffSet, extendedSpeed);
+
+				if (!hasSucceeded)
+				{
+					protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Exception thrown: Parameter set for Extended Speed was not successful", LogType.Error, LogLevel.NoLogging);
+				}
+			}
 		}
 		catch (Exception ex)
 		{
